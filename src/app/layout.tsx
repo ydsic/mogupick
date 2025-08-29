8ab927be513ae8301bb58b8e02a7ed934a66441a
ydsic <ydsic99@gmail.com>
2025-08-29 10:25:06 +0900

    style: 다크모드 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,6 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: '#000000', // 다크 모드에 맞춘 검정색
 };
 
 export default function RootLayout({
