d57b4f906decd2ee751b9fd0a7d618fa7897ce78
양아름 <yangareum0130@gmail.com>
2025-09-03 17:57:49 +0900

    feat : 구독, 구독상세 페이지 생성

---
+++ src/app/(main)/subscribe/[id]/page.tsx
@@ -0,0 +1,16 @@
+import HeaderCustom from '@/components/HeaderCustom';
+
+export default function Page() {
+  return (
+    <div>
+      <HeaderCustom showBack title="상세보기" />
+      <div className="bg-gray-100 px-4">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
