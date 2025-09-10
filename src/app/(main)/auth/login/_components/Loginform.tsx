2904700500e78915d46cff97e1e1fbf2beeabd3e
ydsic <ydsic99@gmail.com>
2025-09-11 05:41:21 +0900

    재배포

--- src/app/(main)/auth/login/_components/Loginform.tsx
+++ src/app/(main)/auth/login/_components/Loginform.tsx
@@ -12,7 +12,7 @@ export default function LoginForm() {
   const handleGoogleLogin = () => {
     signIn('google', { callbackUrl: '/' });
   };
-
+  // test
   return (
     <div className="mt-40 flex w-full flex-col items-center gap-3 text-gray-900">
       <button
