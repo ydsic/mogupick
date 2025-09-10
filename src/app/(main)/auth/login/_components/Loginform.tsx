58ca1bcc0eaeb767d2f1a19a75fc4c02ad94c834
양아름 <yangareum0130@gmail.com>
2025-09-10 13:18:13 +0900

    fix: 로그인 404에러 이슈 처리

--- src/app/(main)/auth/login/_components/Loginform.tsx
+++ src/app/(main)/auth/login/_components/Loginform.tsx
@@ -38,22 +38,26 @@ export default function LoginForm() {
 
   return (
     <div className="mt-40 flex w-full flex-col items-center gap-3 text-gray-900">
-      <Link
-        href={buildUrl('/oauth2/authorization/kakao', 'oauth')}
+      <button
+        onClick={() => {
+          window.location.href = buildUrl('/oauth2/authorization/kakao', 'oauth');
+        }}
         // onClick={() => void signIn('kakao', { callbackUrl })}
         className="flex w-full items-center justify-center gap-1 rounded-xs bg-amber-300 py-3 font-medium"
       >
         <KakaoIcon />
         <span>카카오로 시작하기</span>
-      </Link>
-      <Link
-        href={buildUrl('/oauth2/authorization/google', 'oauth')}
+      </button>
+      <button
+        onClick={() => {
+          window.location.href = buildUrl('/oauth2/authorization/google', 'oauth');
+        }}
         // onClick={() => void signIn('google', { callbackUrl })}
         className="flex w-full items-center justify-center gap-1 rounded-xs border border-gray-400 py-3 font-medium"
       >
         <GoogleIcon />
         <span>구글로 시작하기</span>
-      </Link>
+      </button>
     </div>
   );
 }
